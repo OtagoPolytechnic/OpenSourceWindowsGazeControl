cee5d02f1b5739de689b18e4662c4d2e4f705483
hornoo <rchrdhorne1@gmail.com>
2016-03-22 12:51:47 +1300

    Copied developer code works but dont understant it yet

--- XYPointTrack/XYPointTrack/Program.cs
+++ XYPointTrack/XYPointTrack/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EyeXFramework;
+using Tobii.EyeX.Framework
 
 namespace XYPointTrack
 {
@@ -10,6 +12,24 @@ namespace XYPointTrack
     {
         static void Main(string[] args)
         {
+
+            using (var eyeXHost = new EyeXHost())
+            {
+                // Create a data stream: lightly filtered gaze point data.
+                // Other choices of data streams include EyePositionDataStream and FixationDataStream.
+                using (var lightlyFilteredGazeDataStream = eyeXHost.CreateGazePointDataStream(GazePointDataMode.LightlyFiltered))
+                {
+                    // Start the EyeX host.
+                    eyeXHost.Start();
+
+                    // Write the data to the console.
+                    lightlyFilteredGazeDataStream.Next += (s, e) => Console.WriteLine("Gaze point at ({0:0.0}, {1:0.0}) @{2:0}", e.X, e.Y, e.Timestamp);
+
+                    // Let it run until a key is pressed.
+                    Console.WriteLine("Listening for gaze data, press any key to exit...");
+                    Console.In.Read();
+                }
+            }
 
 
         }
