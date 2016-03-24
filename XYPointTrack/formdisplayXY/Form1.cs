b3956c808ab6cb54da2c01be109eb78175f25d5d
hornoo <rchrdhorne1@gmail.com>
2016-03-24 13:00:57 +1300

    add v2 xy point cap

---
+++ XYPointTrack/formdisplayXY/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EyeXFramework;
+using Tobii.EyeX.Framework;
+
+namespace formdisplayXY
+{
+    public partial class Form1 : Form
+    {
+        EyeXHost IXHost;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            IXHost = new EyeXHost();
+
+            //IXHost.Start();
+
+            var stream = IXHost.CreateGazePointDataStream(GazePointDataMode.LightlyFiltered);
+
+            stream.Next +=
+                 
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.button1.Click += new EventHandler(bob);
+        }
+
+        private void lbX_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bob(object fred, EventArgs jim)
+        {
+           // MessageBox.Show("heres bob");
+           // jim.
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+         
+        }
+
+        
+      /*  private void xyObserver(object sender, GazePointEventArgs e)
+        {
+           // lbX.Text = e.X.ToString("F2");
+          //  lbY.Text = e.Y.ToString("F2");
+
+            //Console.WriteLine("y-Axis" + e.Y.ToString());
+           // Console.WriteLine("x- axis " +  e.X.ToString());
+            
+        } */
+
+        
+
+
+
+    }
+}
--- consolexyv2/consolexyv2/Program.cs
+++ consolexyv2/consolexyv2/Program.cs
@@ -1,26 +1,24 @@
-﻿using System;
+﻿using EyeXFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using EyeXFramework;
 using Tobii.EyeX.Framework;
 
-namespace XYPointTrack
+namespace consolexyv2
 {
     class Program
     {
-
-
-        public void displayEyeGaze(object sender, GazePointEventArgs e)
+        public static void CustomGazeHandler(object eventRaiser, GazePointEventArgs ge)
         {
-            Console.WriteLine(e.X + " " + e.Y);
+            Console.WriteLine("My custom handler say: " + ge.X + ", " + ge.Y);
         }
+
 
 
         static void Main(string[] args)
         {
-
             using (var eyeXHost = new EyeXHost())
             {
                 // Create a data stream: lightly filtered gaze point data.
@@ -34,9 +32,16 @@ namespace XYPointTrack
 
                     //Create the Deligate
 
-                    EventHandler<GazePointEventArgs> displaydel = new EventHandler<GazePointEventArgs>(displayEyeGaze);
+                    EventHandler<GazePointEventArgs> displaydel = new EventHandler<GazePointEventArgs>(CustomGazeHandler);
 
                     lightlyFilteredGazeDataStream.Next += displaydel;
+
+                   // lightlyFilteredGazeDataStream.Next += (s, e) => Console.WriteLine("Gaze point at ({0:0.0}, {1:0.0})", e.X, e.Y);
+
+                    //eyeXHost.UserPresenceChanged += (s, e) => Console.WriteLine("changed");
+                    //var fixationStream = eyeXHost.CreateFixationDataStream(FixationDataMode.Slow);
+
+                    //fixationStream.Next += (s, e) => Console.WriteLine("you fixated at x " + e.X + "y " + e.Y);
 
                     // Let it run until a key is pressed.
                     Console.WriteLine("Listening for gaze data, press any key to exit...");
@@ -44,7 +49,11 @@ namespace XYPointTrack
                 }
             }
 
+        }
 
+        public void displayEyeGaze(object sender, GazePointEventArgs e)
+        {
+            Console.WriteLine(e.X + " " + e.Y);
         }
     }
 }
