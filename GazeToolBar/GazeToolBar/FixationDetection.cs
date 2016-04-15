cbcde6857daa71da28253c8c1aa50bb081fb7384
hornoo <rchrdhorne1@gmail.com>
2016-04-15 12:02:57 +1200

    Mouse left click code added

--- GazeToolBar/GazeToolBar/FixationDetection.cs
+++ GazeToolBar/GazeToolBar/FixationDetection.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EyeXFramework;
+using Tobii.EyeX.Framework;
+using Tobii.EyeX.Client;
 
 namespace GazeToolBar
 {
---
+++ GazeToolBar/GazeToolBar/VirtualMouse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GazeToolBar
+{
+    public class VirtualMouse
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        //This simulates a left mouse click
+        public static void LeftMouseClick(int xpos, int ypos)
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+        }
+    }
+}
