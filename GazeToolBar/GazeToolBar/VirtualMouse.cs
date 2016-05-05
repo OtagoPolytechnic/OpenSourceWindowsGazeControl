6f78f62b8ded28e2c3c9b9fae323be1b912200d5
hornoo <rchrdhorne1@gmail.com>
2016-05-05 16:20:43 +1200

    taking a break

--- GazeToolBar/GazeToolBar/VirtualMouse.cs
+++ GazeToolBar/GazeToolBar/VirtualMouse.cs
@@ -6,29 +6,36 @@ using System.Threading.Tasks;
 
 namespace GazeToolBar
 {
-    public static class VirtualMouse
+    public class VirtualMouse
     {
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
 
+
         public const int MOUSEEVENTF_LEFTDOWN = 0x02;
         public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        
 
 
         
 
         //This simulates a left mouse click
-        public static void LeftMouseClick(int xpos, int ypos)
+        public void LeftMouseClick(int xpos, int ypos)
         {
+
+            
+
+
             mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
             Console.WriteLine("LeftMouseClick X" + xpos + " Y" + ypos);
            // mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
         }
 
-        public static void LeftDoubleMouseClick(int xpos, int ypos)
+        public void LeftDoubleMouseClick(int xpos, int ypos)
         {
-            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
-            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
             mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
             mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
         }
