7acc1d1a090c3801a7dbe9dfa9a58a0723dcca7b
Sam-Bates <batez1993@gmail.com>
2016-05-25 11:29:51 +1200

    Working magnifier version. No click

--- GazeToolBar/ZoomLens.cs
+++ GazeToolBar/ZoomLens.cs
@@ -12,30 +12,18 @@ namespace GazeToolBar
 {
     public partial class ZoomLens : Form
     {
-
-        const int LENSSIZE = 1200;// how big the actual lens is
         const int ZOOMLEVEL = 2;// this is controls how far the lens will zoom in
         int x, y;
         Graphics graphics;
         Bitmap bmpScreenshot;
-        delegate void SetFormDelegate(int x, int y);
-        FixationDetection fixationWorker;
-        public ZoomLens(FixationDetection fixationWorker)
+        delegate void SetFormDelegate(int x, int y);;
+        public ZoomLens()
         {
-
-            //get gaze coordinates
-            //find the offset based on how big the screenshot will be
-            //take a screenshot at the coordinates
-            //make a form %percentage size of the screenshot %percentage = the zoom percentage
-            //off set the form so it is above the original coordinates
             //wait to see where the user is looking
             //translate where the user looked on the form to 
             InitializeComponent();
-            this.fixationWorker = fixationWorker;
-            //this.x = x;
-            //this.y = y;
-            this.Width = LENSSIZE;//setting the lens size
-            this.Height = LENSSIZE;
+            this.Width = Screen.PrimaryScreen.Bounds.Width / 4;//setting the lens size
+            this.Height = Screen.PrimaryScreen.Bounds.Height / 4;
 
             bmpScreenshot = new Bitmap(this.Width / ZOOMLEVEL, this.Height / ZOOMLEVEL);//set bitmap to same size as the lens
             graphics = this.CreateGraphics();
@@ -62,17 +50,16 @@ namespace GazeToolBar
         {
             this.DesktopLocation = new Point(x - (this.Width / 2), y - (this.Height / 2));//set the position of the lens and offset it by it's size /2 to center the lens on the location of the current event
             this.Show();//make lens visible
-            Point lensPoint = new Point();
-            Point empty = new Point(0, 0);
-            lensPoint.X = x; //- (this.Width / 2);//this sets the position on the screen which is being zoomed in. 
-            lensPoint.Y = y; //- (this.Height / 2);
-            graphics.CopyFromScreen(lensPoint.X - (bmpScreenshot.Width /2), lensPoint.Y - (bmpScreenshot.Height /2) , empty.X, empty.Y, bmpScreenshot.Size, CopyPixelOperation.SourceCopy);
 
-            bmpScreenshot = Zoom(bmpScreenshot);
+            graphics.CopyFromScreen(x - (bmpScreenshot.Width /2), y - (bmpScreenshot.Height /2) , 0,0, bmpScreenshot.Size, CopyPixelOperation.SourceCopy);
+
             pictureBox1.Image = bmpScreenshot;
+
             Application.DoEvents();
+
             //somehow check for when another fixation happens on the form?
-            System.Threading.Thread.Sleep(10000);
+
+            System.Threading.Thread.Sleep(10000);//find a better way than sleep
 
 
             //somehow get current looking location 
@@ -80,7 +67,8 @@ namespace GazeToolBar
             //Make the @ the newX and newY Point
             this.Dispose();
         }
-        public Bitmap Zoom(Bitmap bmpScreenshot)
+        
+        public Bitmap Zoom(Bitmap bmpScreenshot)//old method not needed for magnify version
         {
             //RectangleF destinationRect = new RectangleF(150, 20, 1.3f * bmpScreenshot.Width, 1.3f * bmpScreenshot.Height);
             RectangleF cropArea = new RectangleF(0, 0, .5f * bmpScreenshot.Width, .5f * bmpScreenshot.Height);
@@ -90,7 +78,7 @@ namespace GazeToolBar
             Bitmap bmpImage = new Bitmap(bmpScreenshot);
             return bmpImage.Clone(cropArea, bmpImage.PixelFormat);
         }
-        public Point TranslateToDesktop(int x, int y)
+        public Point TranslateToDesktop(int x, int y)//This method translate on form coordinates to desktop coordinates
         {
             Point returnPoint = new Point();
             returnPoint.X = x - (this.Width / 2) * (1 / ZOOMLEVEL) + (this.Left + (this.Width / 2));
