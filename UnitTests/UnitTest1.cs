11710ff1fece744bb042e2fde8838ae9b32cadb2
Sam-Bates <batez1993@gmail.com>
2016-08-06 14:39:06 +1200

    began unit test for zoomer maths, need to revisit later

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ZoomLens_TranslateToDesktop()
+        {
+            GazeToolBar.ZoomLens zoomer = new GazeToolBar.ZoomLens();
+
+            int GazeCoordinate = 100;
+
+            Point expectedP = new Point();
+            
+
+            Point actualP = new Point();
+            actualP.X = zoomer.TranslateToDesktop(GazeCoordinate, GazeCoordinate).X;
+            actualP.Y = zoomer.TranslateToDesktop(GazeCoordinate, GazeCoordinate).Y;
+
+
+
+            //Assert.AreEqual();
+        }
+    }
+}
